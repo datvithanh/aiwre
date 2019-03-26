fd7560b58bc1f017fe850296a3b63abd81b19132
datvithanh98 <datvithanh98@gmail.com>
2019-03-26 19:43:01 +0700

    welcome letter

--- website/models.py
+++ website/models.py
@@ -2,34 +2,35 @@ from django.db import models
 
 #
 class Member(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
-    avatar = models.CharField(max_length=255, null=True)
-    research_interest = models.CharField(max_length=255)
+    name = models.CharField(max_length=1023)
+    address = models.CharField(max_length=1023)
+    email = models.CharField(max_length=1023)
+    phone = models.CharField(max_length=1023)
+    avatar = models.CharField(max_length=1023, null=True)
+    research_interest = models.CharField(max_length=1023)
 
 class Education(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    content = models.CharField(max_length=255)
+    content = models.CharField(max_length=1023)
 
 class ProfessionalExperience(models.Model):
+    timespan = models.CharField(max_length=1023, default='')
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    content = models.CharField(max_length=255)
+    content = models.CharField(max_length=1023)
 
 class Publication(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    content = models.CharField(max_length=255)
+    content = models.CharField(max_length=1023)
 
 #
 class ResearchActivity(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=1023)
+    description = models.CharField(max_length=1023, null=True)
 
 #
 class Project(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=1023)
+    description = models.CharField(max_length=1023, null=True)
 
 #
 class ResearchCooperation(models.Model):
@@ -37,4 +38,4 @@ class ResearchCooperation(models.Model):
 
 #
 class Mission(models.Model):
-    content = models.CharField(max_length=255)
+    content = models.CharField(max_length=1023)
