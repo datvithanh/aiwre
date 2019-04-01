651dae8e48886cdb1124f3a367865861263ce2be
datvithanh98 <datvithanh98@gmail.com>
2019-04-01 18:18:52 +0700

    up

--- website/models.py
+++ website/models.py
@@ -1,41 +1,47 @@
+import datetime
 from django.db import models
 
 #
 class Member(models.Model):
-    name = models.CharField(max_length=1023)
-    address = models.CharField(max_length=1023)
-    email = models.CharField(max_length=1023)
-    phone = models.CharField(max_length=1023)
-    avatar = models.CharField(max_length=1023, null=True)
-    research_interest = models.CharField(max_length=1023)
+    name = models.CharField(max_length=511)
+    address = models.CharField(max_length=511)
+    email = models.CharField(max_length=511)
+    phone = models.CharField(max_length=511)
+    avatar = models.CharField(max_length=511, null=True)
+    research_interest = models.CharField(max_length=511)
 
 class Education(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    content = models.CharField(max_length=1023)
+    college = models.CharField(max_length=511)
+    degree = models.CharField(max_length=511, default='')
+    location = models.CharField(max_length=511, default='')
+    graduation_date = models.DateField(default=datetime.date.today)
 
 class ProfessionalExperience(models.Model):
-    timespan = models.CharField(max_length=1023, default='')
+    timespan = models.CharField(max_length=511, default='')
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    content = models.CharField(max_length=1023)
+    company_name = models.CharField(max_length=511)
+    job_title = models.CharField(max_length=511, default='')
 
 class Publication(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    content = models.CharField(max_length=1023)
+    content = models.CharField(max_length=511)
+    name = models.CharField(max_length=511, default='')
 
 #
 class ResearchActivity(models.Model):
-    name = models.CharField(max_length=1023)
-    description = models.CharField(max_length=1023, null=True)
+    name = models.CharField(max_length=511)
+    description = models.CharField(max_length=511, null=True)
 
 #
 class Project(models.Model):
-    name = models.CharField(max_length=1023)
-    description = models.CharField(max_length=1023, null=True)
+    name = models.CharField(max_length=511)
+    description = models.CharField(max_length=511, null=True)
 
 #
 class ResearchCooperation(models.Model):
-    content = models.CharField(max_length=1023)
+    content = models.CharField(max_length=511)
 
 #
 class Mission(models.Model):
-    content = models.CharField(max_length=1023)
+    content = models.CharField(max_length=511)
