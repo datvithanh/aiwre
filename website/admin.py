9fcebc33d5108407d757de7a23c3c15cc4a72ff3
datvithanh98 <datvithanh98@gmail.com>
2019-07-03 17:29:38 +0700

    update

--- website/admin.py
+++ website/admin.py
@@ -5,8 +5,12 @@ class EducationInline(admin.StackedInline):
     model = Education
     extra = 1
 
-class ProfessionalExperience(admin.StackedInline):
+class ProfessionalExperienceInline(admin.StackedInline):
     model = ProfessionalExperience
+    extra = 1
+
+class ProjectObjectiveInline(admin.StackedInline):
+    model = ProjectObjective
     extra = 1
 
 class PublicationInline(admin.StackedInline):
@@ -17,14 +21,16 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ['name', 'order']
     list_editable = ['order']
     list_display_links = ['name']
-    inlines = [EducationInline, ProfessionalExperience, PublicationInline]
+    inlines = [EducationInline, ProfessionalExperienceInline, PublicationInline]
 
-class Project(admin.ModelAdmin):
+
+
+class ProjectAdmin(admin.ModelAdmin):
     list_display = ['name']
-    inlines = [ProjectObjective]
+    inlines = [ProjectObjectiveInline]
 
 admin.site.register(Member, MemberAdmin)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(ResearchCooperation)
 admin.site.register(ResearchActivity)
 admin.site.register(Mission)
